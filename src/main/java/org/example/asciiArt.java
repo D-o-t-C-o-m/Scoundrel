7fcfad343678393159ac0b080967e153c50dcc21
valkuros@gmail.com <valkuros@gmail.com>
2025-04-12 15:37:55 -0400

    Jar 1.02 with title change

--- src/main/java/org/example/asciiArt.java
+++ src/main/java/org/example/asciiArt.java
@@ -13,10 +13,10 @@ public asciiArt() {}
 public void Title() throws IOException {
 BufferedImage image = new BufferedImage(144, 32, BufferedImage.TYPE_INT_RGB);
 Graphics g = image.getGraphics();
-g.setFont(new Font("Impact", Font.PLAIN, 16));
+g.setFont(new Font("Noto Sans", Font.PLAIN, 14));
 Graphics2D graphics = (Graphics2D) g;
-graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+//graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+//RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 graphics.drawString("Scoundrel", 6, 24);
 ImageIO.write(image, "png", new File("text.png"));
 
